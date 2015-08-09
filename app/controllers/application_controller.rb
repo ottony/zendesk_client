3621559f90517b05369e0297f0242d4c02f53377
Ottony Costa <ottony93@gmail.com>
2015-08-09 03:31:40 -0300

    Creates routers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/submit' do
+    redirect '/obrigado'
+  end
+
+  get '/obrigado' do
+    erb :thanks
+  end
 end
