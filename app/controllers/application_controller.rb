482e3309b67a5a4799d858f75cbcb506b36f91d9
Ottony Costa <ottony93@gmail.com>
2015-08-09 23:17:30 -0300

    Upload file from sinatra

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,30 @@
 class ApplicationController < Sinatra::Base
   get '/' do
+    @client = Client.new email: '', name: ''
     erb :index
   end
 
   post '/submit' do
+    @client = Client.new client_params
+    @client.save
+
     redirect '/obrigado'
   end
 
   get '/obrigado' do
     erb :thanks
+  end
+
+  private
+
+  def client_params
+    selected = params.select { |k, v| permited_params.include? k.to_sym }
+    selected[:curriculum] = params[:curriculum][:tempfile]
+
+    selected.symbolize_keys
+  end
+
+  def permited_params
+    [ :email, :name, :cpf ]
   end
 end
